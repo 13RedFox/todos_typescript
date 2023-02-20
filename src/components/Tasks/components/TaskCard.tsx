87c610ab7701c0be7537e07a794bb5199b9056a6
Serhii Shabala <sergei181989@gmail.com>
2023-02-20 19:14:14 +0200

    :recycle: Fix functionality for adding tasks.

--- src/components/Tasks/components/TaskCard.tsx
+++ src/components/Tasks/components/TaskCard.tsx
@@ -1,5 +1,8 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { AddTaskForm } from '../../../module';
 import { FolderType } from '../../../types/folder.type';
+import { Button } from '../../Button/Button';
 import styles from '../Tasks.module.scss';
 import { TaskListItem } from './TaskListItem';
 
@@ -7,6 +10,14 @@ interface TaskCardProps {
   task: FolderType;
 }
 export const TaskCard: FC<TaskCardProps> = ({ task }): JSX.Element => {
+  const [showTaskForm, setShowTaskForm] = useState<boolean>(false);
+
+  const { id } = useParams();
+
+  const toggleTaskForm = () => {
+    setShowTaskForm((prevState) => !prevState);
+  };
+
   return (
     <div
       key={task.id}
@@ -21,6 +32,19 @@ export const TaskCard: FC<TaskCardProps> = ({ task }): JSX.Element => {
       <ul className={styles.tasks__list}>
         <TaskListItem tasks={task.tasks} />
       </ul>
+      {!showTaskForm && id !== 'all' ? (
+        <Button
+          type='button'
+          icon
+          size='task'
+          color='cancel'
+          className={styles.tasks__btn}
+          onClick={toggleTaskForm}
+        >
+          Новая задача
+        </Button>
+      ) : null}
+      {showTaskForm && <AddTaskForm setShowTaskForm={setShowTaskForm} />}
     </div>
   );
 };
--- src/module/AddTaskForm/AddTaskForm.tsx
+++ src/module/AddTaskForm/AddTaskForm.tsx
@@ -1,30 +1,60 @@
-import { FC } from 'react';
+import { ChangeEvent, Dispatch, FC, FormEvent, SetStateAction, useState } from 'react';
+import { uid } from 'uid';
 import { Button } from '../../components';
+import { TaskType } from '../../types/folder.type';
 import styles from './AddTaskForm.module.scss';
 
-export const AddTaskForm: FC = (): JSX.Element => {
+interface AddTaskFormProps {
+  setShowTaskForm: Dispatch<SetStateAction<boolean>>;
+}
+
+export const AddTaskForm: FC<AddTaskFormProps> = ({ setShowTaskForm }): JSX.Element => {
+  const [inputValue, setInputValue] = useState<string>('');
+  const [task, setTask] = useState<TaskType[]>([]);
+
+  const addTaskFormSubmit = (e: FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+
+    setTask((prevState) => {
+      return [
+        ...prevState,
+        {
+          id: uid(),
+          title: inputValue,
+          isComplete: false,
+        },
+      ];
+    });
+
+    setShowTaskForm(false);
+  };
   return (
-    <form className={styles.form}>
+    <form
+      className={styles.form}
+      onSubmit={addTaskFormSubmit}
+    >
       <input
         type='text'
+        value={inputValue}
         placeholder='Текст задачи'
         className={styles.form__input}
+        onChange={(e: ChangeEvent<HTMLInputElement>) => setInputValue(e.target.value)}
       />
       <div className={styles.form__wrap}>
         <Button
-          type='submit'
           size='task'
           color='add'
           icon={false}
+          type='submit'
           className={styles.form__wrap_btn}
         >
           Добавить задачу
         </Button>
         <Button
-          type='button'
           size='task'
-          color='default'
           icon={false}
+          type='button'
+          color='default'
           className={styles.form__wrap_btn}
         >
           Отмена
